f452a7f97e469a010b39fa2b9ad22f142db96472
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2013-12-10 22:25:54 +0000

    intial commit 2

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Prowl::Application.config.session_store :cookie_store, key: '_Prowl_session'
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Prowl::Application.routes.draw do
+  get "home/index"
+
+  as :user do
+    get "/login" => "devise/sessions#new", :as => :new_user_session
+    post "/login" => "devise/sessions#create", :as => :user_session
+    delete "/logout" => "devise/sessions#destroy", :as => :destroy_user_session
+  end
+
+  devise_for :users, :skip => [:registrations, :sessions]
+
+  resources :users
+  resources :apps
+
+  root :to => "home#index"
+end
