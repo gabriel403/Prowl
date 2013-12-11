a39ccc677647e668f89e80e23e05fbe3beed9d2a
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2013-12-11 22:06:05 +0000

    work on app views

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,17 +1,22 @@
 class AppsController < ApplicationController
   def index
+    @apps = App.all
   end
 
   def show
   end
 
   def new
+    @app = App.new
   end
 
   def edit
+    @app = App.find(params[:id])
   end
 
   def create
+    @app = current_user.apps.create(params[:app].permit(:name, :description))
+    redirect_to user_path(current_user)
   end
 
   def update
