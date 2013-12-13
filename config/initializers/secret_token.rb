5652c58c7f5076127dfda0610fb95a6762ecd465
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2013-12-13 18:43:46 +0000

    PROWL_SECRET_TOKEN

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Prowl::Application.config.secret_key_base = ENV['RAILS_SECRET_TOKEN']
+Prowl::Application.config.secret_key_base = ENV['PROWL_SECRET_TOKEN']
