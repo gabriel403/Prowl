f2fce5d3717ee258241cababbe5830f8a0fb5e5f
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2014-01-23 20:57:39 +0000

    different loggers for dev and prod

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -1,6 +1,5 @@
-# require 'le'
-# require 'logglier'
-# Rails.application.config.logger = Logglier.new("https://logs-01.loggly.com/inputs/bd5fce10-4fba-4433-8b89-ebe68a5c270e/tag/ruby/")
 if Rails.env.production?
   Rails.logger = Le.new('5c703294-66e8-45db-aeba-1e8915b4c20c')
+elsif Rails.env.development?
+  Rails.logger = Le.new('b9259c12-1a1b-4453-a821-3c5eee10807e')
 end
--- lib/tasks/deployment.rb
+++ lib/tasks/deployment.rb
@@ -6,7 +6,11 @@ module Tasks
     @queue = :file_serve
 
     def self.perform(app_id, server_id, deploy_id, force = false)
-      Resque.logger = Le.new('5c703294-66e8-45db-aeba-1e8915b4c20c')
+      if Rails.env.production?
+        Rails.logger = Le.new('5c703294-66e8-45db-aeba-1e8915b4c20c')
+      elsif Rails.env.development?
+        Rails.logger = Le.new('b9259c12-1a1b-4453-a821-3c5eee10807e')
+      end
 
       app    = App.find(app_id)
       server = Server.find(server_id)
