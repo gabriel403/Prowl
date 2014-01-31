1e58c87ee29d716249d6c7c00f260b210b198984
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2014-01-31 17:47:20 +0000

    backticks

--- app/models/deploy_step.rb
+++ app/models/deploy_step.rb
@@ -1,5 +1,5 @@
 class DeployStep < ActiveRecord::Base
-  default_scope order('`order` ASC')
+  default_scope order('order ASC')
   serialize :additional, JSON
   belongs_to :deploy_step_type_option
   belongs_to :app
