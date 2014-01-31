bb45707d8b99c2e1d7a127abe00dd260003e56eb
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2014-01-31 09:34:30 +0000

    db migrations

---
+++ db/migrate/20140131084724_lowercase_deploy_status.rb
@@ -0,0 +1,9 @@
+class LowercaseDeployStatus < ActiveRecord::Migration
+  def self.up
+    execute "UPDATE `deploys` SET `status` = lower(`status`)"
+  end
+
+  def self.down
+    # not implemented
+  end
+end
---
+++ db/migrate/20140131093054_add_user_id_to_all_deploy.rb
@@ -0,0 +1,9 @@
+class AddUserIdToAllDeploy < ActiveRecord::Migration
+  def up
+    execute "UPDATE `deploys` SET `user_id` = 1"
+  end
+
+  def down
+    # not implemented
+  end
+end
