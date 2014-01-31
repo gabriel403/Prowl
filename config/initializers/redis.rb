6b40d1483395be954609fb1328ce22dea5af0328
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2014-01-31 18:54:40 +0000

    redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,5 @@
-redis_config = YAML.load_file("#{Rails.root}/config/redis.yml") 
+rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
+rails_env = ENV['RAILS_ENV'] || 'development'
+
+redis_config = YAML.load_file("#{rails_root}/config/redis.yml") 
 Resque.redis = Redis.new(redis_config[Rails.env])
