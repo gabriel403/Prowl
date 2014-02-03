4304ec6c9cb755c3c5c81586d3f1b8c35ddbdb75
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2014-02-03 13:18:58 +0000

    dammit

--- db/migrate/20140203074319_add_sudo_auth_to_server.rb
+++ db/migrate/20140203074319_add_sudo_auth_to_server.rb
@@ -1,6 +1,6 @@
 class AddSudoAuthToServer < ActiveRecord::Migration
   def change
-    # add_column :servers, :can_sudo, :boolean
-    # add_column :servers, :sudo_password, :string
+    add_column :servers, :can_sudo, :boolean
+    add_column :servers, :sudo_password, :string
   end
 end
