75235fd248dbdf53322e5cc4ce2a7a7ecf19cb1d
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2014-02-11 10:42:24 +0000

    initial rework for environments

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,6 +1,7 @@
 class Server < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :apps
+  has_and_belongs_to_many :environments
   belongs_to :authentication_type
   has_many :deploys
 end
---
+++ db/migrate/20140210214600_create_apps_envs.rb
@@ -0,0 +1,9 @@
+class CreateAppsEnvsServers < ActiveRecord::Migration
+  def change
+    create_table :apps_environments do |t|
+      t.integer :app_id
+      t.integer :environment_id
+    end
+    add_index :apps_environments, [:app_id, :environment_id], :unique => true
+  end
+end
