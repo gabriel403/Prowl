ec080491f27387cd061bc7917189b259b53cb144
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2014-02-18 22:35:14 +0000

    Removed app from models

--- app/models/deploy.rb
+++ app/models/deploy.rb
@@ -1,6 +1,5 @@
 class Deploy < ActiveRecord::Base
   has_many :deploy_options
-  belongs_to :app
   belongs_to :environment
   belongs_to :server
   belongs_to :user
--- app/models/deploy_step.rb
+++ app/models/deploy_step.rb
@@ -2,6 +2,5 @@ class DeployStep < ActiveRecord::Base
   default_scope { order('"order" ASC') }
   serialize :additional, JSON
   belongs_to :deploy_step_type_option
-  belongs_to :app
   belongs_to :environment
 end
