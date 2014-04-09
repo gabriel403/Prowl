18c34039c52d19c1f7b555a9a324e77a192d9f83
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2014-04-09 21:39:41 +0100

    added branch name handling

---
+++ db/migrate/20140409154306_add_branch_option_type.rb
@@ -0,0 +1,9 @@
+class AddBranchOptionType < ActiveRecord::Migration
+  def up
+	DeployOptionType.create! :name => "branch_name"
+  end
+
+  def down
+    DeployOptionType.destroy_all(name: "branch_name")
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140315183622) do
+ActiveRecord::Schema.define(version: 20140409154306) do
 
   create_table "_environments_old_20140214", force: true do |t|
     t.string   "name"
@@ -127,12 +127,12 @@ ActiveRecord::Schema.define(version: 20140315183622) do
   add_index "servers", ["user_id"], name: "index_servers_on_user_id"
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                   default: "",    null: false
+    t.string   "encrypted_password",      default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",           default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -141,16 +141,20 @@ ActiveRecord::Schema.define(version: 20140315183622) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,  null: false
+    t.integer  "failed_attempts",         default: 0,     null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
     t.string   "authentication_token"
+    t.string   "authy_id"
+    t.datetime "last_sign_in_with_authy"
+    t.boolean  "authy_enabled",           default: false
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token"
+  add_index "users", ["authy_id"], name: "index_users_on_authy_id"
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
