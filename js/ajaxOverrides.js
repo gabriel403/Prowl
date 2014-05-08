af17b834568f0169eb86f16022cb9732f2afb793
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2014-05-08 16:20:57 +0100

    sod csrf

--- js/ajaxOverrides.js
+++ js/ajaxOverrides.js
@@ -1,15 +1,3 @@
-$(document).ajaxComplete(function(e, xhr, options) {
-  var csrf_param = xhr.getResponseHeader('X-CSRF-Param');
-  var csrf_token = xhr.getResponseHeader('X-CSRF-Token');
-
-  if (csrf_param) {
-    $('meta[name="csrf-param"]').attr('content', csrf_param);
-  }
-  if (csrf_token) {
-    $('meta[name="csrf-token"]').attr('content', csrf_token);
-  }
-});
-
 $( document ).ajaxError(function(e, xhr, settings, error) {
   if ('Unauthorized' == error) {
     $.userLoggedIn = false;
