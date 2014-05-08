a7d9e188e73f5bfae23f6930ab8593787f4c66fa
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2014-05-08 16:20:45 +0100

    tweaks for deauthed

--- js/display.js
+++ js/display.js
@@ -1,6 +1,11 @@
 $( document ).ready(function(){
-  $( document ).on("prowl:user:authenticated", function(userObject){
+  $( document ).on("prowl:user:authenticated", function(event){
     $("#marketing").addClass("hide");
     $("#mainArea").removeClass("hide");
+  });
+
+  $( document ).on("prowl:user:deauthed", function(event){
+    $("#mainArea").addClass("hide");
+    $("#marketing").removeClass("hide");
   });
 });
