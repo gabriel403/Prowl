c19ed9adfc916e038506cfe40cf69fde659b112d
Gabriel Baker <gabriel@autonomicpilot.co.uk>
2014-05-09 07:10:31 +0100

    fix model test

--- app/models/organisation_user.rb
+++ app/models/organisation_user.rb
@@ -2,6 +2,6 @@ class OrganisationUser < ActiveRecord::Base
   belongs_to :user
   belongs_to :organisation
   belongs_to :access_level
-  # validates  :user, uniqueness: true
+  validates  :user, uniqueness: true
   # validates  :user, uniqueness: { scope: :organisation }
 end
